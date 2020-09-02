5e4beed9e94db39518755d559aec77e7f4341397
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-09-02 17:27:19 +0200

    [TASK] Set version to 0.4.1-dev

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -27,7 +27,7 @@ author = u'Chris Müller'
 # The short X.Y version
 version = u'0.4'
 # The full version, including alpha/beta/rc tags
-release = u'0.4.0'
+release = u'0.4.1-dev'
 
 
 # -- General configuration ---------------------------------------------------
