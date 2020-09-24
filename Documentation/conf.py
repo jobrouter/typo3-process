19a461be2a4c57b88f2100b5d8556c1b2378f0da
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-09-24 17:49:19 +0200

    [TASK] Set version to 0.5.0-dev

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -25,9 +25,9 @@ copyright = u'2020, Chris Müller'
 author = u'Chris Müller'
 
 # The short X.Y version
-version = u'0.4'
+version = u'0.5'
 # The full version, including alpha/beta/rc tags
-release = u'0.4.1'
+release = u'0.5.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
