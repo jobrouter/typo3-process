e64b33942d1b71fba7ebe123c4d99c31c66e9cc6
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-02-13 16:47:49 +0100

    [TASK] Set version to 0.5.1-dev

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -27,7 +27,7 @@ author = u'Chris Müller'
 # The short X.Y version
 version = u'0.5'
 # The full version, including alpha/beta/rc tags
-release = u'0.5.0'
+release = u'0.5.1-dev'
 
 
 # -- General configuration ---------------------------------------------------
