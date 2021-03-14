aa1313000634699e99791946e5c3f9de726471fc
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-03-14 15:31:31 +0100

    [TASK] Require stable versions of JobRouter extensions

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '10.4.11-10.4.99',
             'form' => '',
             'jobrouter_base' => '1.0.0-1.99.99',
-            'jobrouter_connector' => '0.12.0-1.99.99',
+            'jobrouter_connector' => '1.0.0-1.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
