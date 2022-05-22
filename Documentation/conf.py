205b31da9d7dde8e994a116d976d8c21ab620f1a
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-05-22 14:48:10 +0200

    [TASK] Set version to 1.2.0-dev

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -25,9 +25,9 @@ copyright = u'since 2020 by Chris Müller'
 author = u'Chris Müller'
 
 # The short X.Y version
-version = u'1.1'
+version = u'1.2'
 # The full version, including alpha/beta/rc tags
-release = u'1.1.1'
+release = u'1.2.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
