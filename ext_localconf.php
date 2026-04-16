e1e2fdb060bfa512636501ec582a5a2f59ba1e41
Chris Müller <coding@brotkrueml.dev>
2026-04-16 10:45:34 +0200

    Use automatic import form configuration in TYPO3 v14

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,24 +2,29 @@
 
 declare(strict_types=1);
 
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') || die();
 
-ExtensionManagementUtility::addTypoScriptSetup('
-    module.tx_form {
-      settings {
-        yamlConfigurations {
-          1581265491 = EXT:jobrouter_process/Configuration/Yaml/FormSetup.yaml
+// @todo Remove once compatibility with TYPO3 v13 is dropped
+if ((new Typo3Version())->getMajorVersion() < 14) {
+    ExtensionManagementUtility::addTypoScriptSetup('
+        module.tx_form {
+          settings {
+            yamlConfigurations {
+              1581265491 = EXT:jobrouter_process/Configuration/Form/Base/Finishers/JobRouterStartInstance.yaml
+            }
+          }
         }
-      }
-    }
 
-    plugin.tx_form {
-      settings {
-        yamlConfigurations {
-          1581265491 = EXT:jobrouter_process/Configuration/Yaml/FormSetup.yaml
+        plugin.tx_form {
+          settings {
+            yamlConfigurations {
+              1581265491 = EXT:jobrouter_process/Configuration/Form/Base/Finishers/JobRouterStartInstance.yaml
+            }
+          }
         }
-      }
-    }
-');
+    ');
+}
+
